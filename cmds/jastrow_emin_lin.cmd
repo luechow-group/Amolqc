6315541227e8da1067166acdf38c19a71adf7871
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2022-04-29 16:33:59 +0200

    Added new names for optimization methods.

--- cmds/jastrow_emin_lin.cmd
+++ cmds/jastrow_emin_lin.cmd
@@ -15,7 +15,7 @@ x$qmc(step_stride=10,acc_size=1k,block_len=200,accept_ratio=.5,discard=1,move=um
 vmc,accumulate)
 $sample(remove_outliers,no_replace)
 x$optimize_parameters(target_E=REQUIRED,target_var=0.,eq_iter=5,max_ev=5,ev_sample_size=100,cffac=0.,optmode=1,
-params=jastrow,energy_min,method=eminlin,eq_call=jelin,write_wf)
+params=jastrow,energy_min,method=lin,eq_call=jelin,write_wf)
 $sample(change_size,new_size=1,last)
 x$qmc(accept_ratio=.5,
 vmc,discard_all,steps=1500,block_len=500)
--- cmds/jastrow_emin_lm.cmd
+++ cmds/jastrow_emin_lm.cmd
@@ -14,7 +14,7 @@ x$qmc(step_stride=10,acc_size=1k,block_len=200,accept_ratio=.5,discard=1,move=um
 vmc,accumulate)
 $sample(remove_outliers,no_replace)
 x$optimize_parameters(eq_iter=5,nu=.001,delta_f_min=.0001,max_var=50.,optmode=1,
-params=jastrow,energy_min,method=lm_newton,eq_call=jelm,write_wf)
+params=jastrow,energy_min,method=snr,eq_call=jelm,write_wf)
 $sample(change_size,new_size=1,last)
 x$qmc(accept_ratio=.5,
 vmc,discard_all,steps=1500,block_len=500)
